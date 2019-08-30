c39675c63137476c8f4c70668bd25a679fe70521
mahdihassani9374 <mahdihassani9374@gmail.com>
2019-08-30 22:36:48 +0430

    componnet top product

--- docs/js/script.js
+++ docs/js/script.js
@@ -14,4 +14,38 @@ $(function () {
   setTimeout(function () {
    // toastr.success('سلام خوش آمدید ')
   }, 2000);
+
+  $('.slick__item').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  $('[data-role="slick-left"]').on('click',function(){
+    $('.slick__item').slick('slickPrev');
+  })
+  $('[data-role="slick-right"]').on('click',function(){
+    $('.slick__item').slick('slickNext');
+  })
 })
